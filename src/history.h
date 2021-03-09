144bf62bcfa68de6ef4ff4983c183236a031a3ba
vcabral001 <77982111+vcabral001@users.noreply.github.com>
2021-03-09 14:23:54 -0700

    Update history.h

--- src/history.h
+++ src/history.h
@@ -12,21 +12,41 @@ typedef struct s_List {
 } List;
 
 /* Initialize the linked list to keep the history. */
-List* init_history();
+List* init_history(){
+  list.size = size;
+  list.data = data;
+}
 
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str);
+void add_history(List *list, char *str){
+  int size = arraylist_get_size(*list);
+  char *new_data;
+  
+  new_data = realloc(list.data, (size + 1)*sizeof new_data[0]);
+  
+  if(new_data){
+    new_data[size] = value;
+    arraylist_set_data_collection(list, new_data);
+    ++list.size;
+  }
+}
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
-char *get_history(List *list, int id);
+char *get_history(List *list, int id){
+  return id;
+}
 
 / *Print the entire contents of the list. */
-void print_history(List *list);
+void print_history(List *list){
+  for(int i = 0; i < list; i++){
+    printf(%c, list[i]);
+  }
+}
 
 / *Free the history list and the strings it references. */
 void free_history(List *list);
